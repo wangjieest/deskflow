3b4fa06ad0d7b4daf77919ec8bf26f6ec64a7d09
jeffry-wang <jeffry.wang@supercell.com>
2026-04-23 19:05:42 +0800

    fix: disable auto update check by default, auto-detect P2P packet format
    - Disable update check prompt on first launch (AutoDeskflow doesn't
      have an update server)
    - P2P client auto-detects whether server sends with or without
      4-byte length prefix (handles both ClipboardTransferServer variants)

--- src/lib/gui/MainWindow.cpp
+++ src/lib/gui/MainWindow.cpp
@@ -646,14 +646,9 @@ void MainWindow::open()
   const auto kCriticalDialogDelay = 100;
   QTimer::singleShot(kCriticalDialogDelay, this, &messages::raiseCriticalDialog);
 
-  if (!Settings::value(Settings::Gui::AutoUpdateCheck).isValid()) {
-    Settings::setValue(Settings::Gui::AutoUpdateCheck, messages::showUpdateCheckOption(this));
-  }
-
+  // Auto update check disabled by default for AutoDeskflow
   if (Settings::value(Settings::Gui::AutoUpdateCheck).toBool()) {
     m_versionChecker.checkLatest();
-  } else {
-    qDebug() << "skipping check for new version, disabled";
   }
 
   if (Settings::value(Settings::Gui::AutoStartCore).toBool()) {
